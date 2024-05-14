658efbdff9d35ce57eaa483a753a307a352d1a25
amaaaannn <amanmuhammed7007@gmail.com>
2024-05-14 16:05:12 +0530

    to do service added

--- lib/screens/todo main.dart
+++ lib/screens/todo main.dart
@@ -1,15 +1,22 @@
 import 'dart:ui';
 
+import 'package:anonymous/SERVICES/TODO_SERVICES.dart';
 import 'package:anonymous/screens/ADD%20TASK.dart';
 import 'package:anonymous/screens/edit.dart';
 import 'package:flutter/material.dart';
 
-class todo extends StatelessWidget {
+class todo extends StatefulWidget {
   const todo({super.key});
 
   @override
+  State<todo> createState() => _todoState();
+}
+
+class _todoState extends State<todo> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Color(0xffe7c6ff),
+    return Scaffold(
+      backgroundColor: Color(0xffe7c6ff),
       appBar: AppBar(
         automaticallyImplyLeading: false,
         backgroundColor: Color(0xffc8b6ff),
@@ -25,40 +32,61 @@ class todo extends StatelessWidget {
         ],
       ),
       body: ListView.builder(
-        itemCount: 6,
+        itemCount: todoList.length,
         itemBuilder: (context, index) => Card(
           child: ListTile(
             title: Text(
-              "TODO TITLE",
-              style: TextStyle(fontSize: 24, color: Color(0xffc8b6ff),fontWeight:FontWeight.bold),
+              todoList[index]['title'],
+              style: TextStyle(
+                  fontSize: 24,
+                  color: Color(0xffc8b6ff),
+                  fontWeight: FontWeight.bold),
             ),
-            subtitle: Text("TODO SUBTITLE",style: TextStyle( fontSize:18,color: Colors.black),
+            subtitle: Text(
+                todoList[index]['detail'],
+              style: TextStyle(fontSize: 18, color: Colors.black),
             ),
             trailing: Row(
               mainAxisSize: MainAxisSize.min,
               children: [
-                IconButton(onPressed:() {
-                  Navigator.push(context,MaterialPageRoute(builder:  (context) =>EDIT()),);
-                }
-                , icon: Icon(Icons.mode_edit_outline_outlined),),
-                IconButton(onPressed: () {},
-                icon: Icon(Icons.delete_rounded),),
-                IconButton(onPressed:() {} ,
-                icon: Icon(Icons.verified_outlined),)
+                IconButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => EDIT()),
+                    );
+                  },
+                  icon: Icon(Icons.mode_edit_outline_outlined),
+                ),
+                IconButton(
+                  onPressed: () {  todoList.removeAt(index);
+                  print(todoList);
+                  setState(() {
 
+                  });},
+                  icon: Icon(Icons.delete_rounded),
+                ),
+                IconButton(
+                  onPressed: () {
 
+                  },
+                  icon: Icon(Icons.verified_outlined),
+                )
               ],
             ),
           ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(onPressed: () {
-        Navigator.push(context, MaterialPageRoute(builder:(context) => ADDTASK(),));
-      },
-
-     child: Icon(Icons.add),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => ADDTASK(),
+              ));
+        },
+        child: Icon(Icons.add),
       ),
-
     );
   }
 }
